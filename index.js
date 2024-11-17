37ff0add0ee644df995e93342413365377550cf5
Goosegi <helloadobe2@gmai;.com>
2024-11-17 20:23:31 +0300

    add menu

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const token = process.env.BOT_API_KEY
 const bot = new TelegramBot(token, { polling: true });
 const commands = [
   {
-    command: "/govnovoz",
-    description: "На зиле"
+    command: "/menu",
+    description: "меню команд "
   },
   {
-    command: "/kall",
-    description: "Заполнен чан"
-  },
+    command: "/help",
+    description: "помощь"
+  }
 ];
 
 
@@ -22,16 +22,55 @@ const commands = [
 // });
 
 bot.on('text', async msg => {
+  try {
+    const chatId = msg.chat.id;
+    if (msg.text == '/menu') {
+      await bot.sendMessage(chatId, 'меню бота', {
+        reply_markup: {
+          keyboard: [
+            ['🚘Петрович', '🏁Врубай🏁'],
+            ['🐺Насос🐺', '🚗Говновоз!🚗'],
+            ['🐺🏁Закончить смену🏁🐺']
+          ],
+          resize_keyboard: true
+        }
+      })
+    } else if (msg.text == '/help') {
+      await bot.sendMessage(chatId, 'поможет только это \n https://www.youtube.com/watch?v=dJhKEAWn2Zo')
+    }
+
+  }
+  catch (error) {
+    console.log(error);
+  }
+})
+
+bot.on('text', async msg => {
 
   try {
     const chatId = msg.chat.id;
     switch (msg.text) {
-      case '/govnovoz':
-        bot.sendMessage(chatId, 'https://www.youtube.com/watch?v=FQtPSH2ibYQ')
+      case '🚘Петрович':
+        await bot.sendMessage(chatId, 'https://www.youtube.com/watch?v=FQtPSH2ibYQ')
         break;
-      case '/kall':
-        bot.sendMessage(chatId, 'https://www.youtube.com/watch?v=dJhKEAWn2Zo')
+      case '🏁Врубай🏁':
+        await bot.sendMessage(chatId, 'https://www.youtube.com/watch?v=cuS7EvgwaTI')
         break;
+      case '🐺Насос🐺':
+        await bot.sendMessage(chatId, 'https://www.youtube.com/watch?v=t_6RmX-9EZo')
+        break;
+      case '🚗Говновоз!🚗':
+        await bot.sendMessage(chatId, 'https://www.youtube.com/watch?v=zjOq73DH3uU')
+        break;
+        case '🐺🏁Закончить смену🏁🐺':
+        await bot.sendMessage(chatId, 'До скорой встречи, работяга!!!', {
+          reply_markup:  {
+            remove_keyboard: true
+          }
+        })
+        break;
+
+
     }
   }
   catch (error) {
